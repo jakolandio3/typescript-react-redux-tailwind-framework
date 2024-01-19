e612e74269f47ac4ec8e9b47a0aea27e663bd004
Jakob Douglas <jakobdouglas1@gmail.com>
2024-01-19 15:19:04 +1030

    chore: config dependencies & chnage to docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import React from 'react';
+import { ReactNode, useState } from 'react';
 import './App.css';
 
-function App() {
-	return <div className='App bg-red-400'>hello</div>;
+interface AppProps {
+	color: string;
+	children?: ReactNode;
+}
+
+function App({ color, children = 'hello' }: AppProps): React.JSX.Element {
+	const [counter, setCounter] = useState(0);
+	function IncNum(): void {
+		setCounter(counter + 1);
+	}
+	function DecNum(): void {
+		setCounter((counter) => counter - 1);
+	}
+	return (
+		<div className='App bg-red-400'>
+			<button onClick={IncNum}>+1</button>
+			<p> {counter} </p>
+			{children}
+			{color}
+			<button onClick={DecNum}>-1</button>
+		</div>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+interface AppProps {
+	color?: string;
+	//optional property
+	milkos?: string;
+}
+interface InitialState {
+	counter: number;
+}
+//either we set a state in the contructor using an initial state interface or we do it in a class as a property
+class TestApp extends React.Component<AppProps, InitialState> {
+	constructor(props: AppProps) {
+		super(props);
+		this.state = { counter: 0 };
+	}
+	inc = (): void => {
+		this.setState({ counter: this.state.counter + 1 });
+	};
+	dec = (): void => {
+		this.setState({ counter: this.state.counter - 1 });
+	};
+	render(): React.ReactNode {
+		return (
+			<div className=''>
+				<button onClick={this.inc}>Incriment</button>
+				{this.state.counter}
+				<button onClick={this.dec}>Decriment</button>
+			</div>
+		);
+	}
+}
+//we can define props in super or as a type parameter as a generic
+// class TestApp extends React.Component {
+// 	constructor(public props: any) {
+// 		super(props);
+// 	}
+// 	render(): React.ReactNode {
+// 		return (
+// 			<div className=' text-red-500 bg-blue-400'>
+// 				{this.props.color}Hello there young man
+// 			</div>
+// 		);
+// 	}
+// }
+root.render(
+	<React.StrictMode>
+		<App color='red'>
+			<TestApp />
+			<TestApp />
+		</App>
+		<TestApp color='red' />
+	</React.StrictMode>
+);
